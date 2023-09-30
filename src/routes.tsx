87226cf14c704ed19fdd1a6cbbcb651f408d40ba
Luis Melo <luis.melo@hytechgizmo.com>
2023-09-30 12:03:19 -0400

    added 404 and 500 routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -6,6 +6,8 @@ import Inspect_Atoms from '@/routes/inspect/atoms';
 import Inspect_Molecules from '@/routes/inspect/molecules';
 import Inspect_Organisms from '@/routes/inspect/organisms';
 import Inspect_Widgets from '@/routes/inspect/widgets';
+import Error_404 from '@/routes/404';
+import Error_500 from '@/routes/500';
 
 const AppRoutes = () => (
 	<Routes>
@@ -16,6 +18,8 @@ const AppRoutes = () => (
 		<Route path='/inspect/molecules' element={<Inspect_Molecules />} />
 		<Route path='/inspect/organisms' element={<Inspect_Organisms />} />
 		<Route path='/inspect/widgets' element={<Inspect_Widgets />} />
+		<Route path='*' element={<Error_404 />} />
+		<Route path='/500' element={<Error_500 />} />
 	</Routes>
 );
 
