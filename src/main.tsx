d09b245f4f00afd13f1603e9903652dfbbbfc779
Luis Melo <luis.melo@hytechgizmo.com>
2023-09-30 10:30:32 -0400

    moved routes to its own file

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,10 @@ import '@/commons/styles/icons.scss';
 import '@/commons/styles/tailwind.scss';
 import '@/commons/styles/main.scss';
 
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-
+import { BrowserRouter as Router } from 'react-router-dom';
 import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
-import Index from './views/app/index';
-import Dashboard from './views/app/dashboard';
-import Login from './views/auth/login';
-import Inspect_Atoms from './views/inspect/atoms';
-import Inspect_Molecules from './views/inspect/molecules';
-import Inspect_Organisms from './views/inspect/organisms';
-import Inspect_Widgets from './views/inspect/widgets';
+import AppRoutes from '@/routes'; // Import the AppRoutes component
 
 const rootElement: HTMLElement | null = document.getElementById('root');
 
@@ -26,15 +19,8 @@ function renderApp(rootElement: HTMLElement | null) {
 		<React.StrictMode>
 			<Router>
 				<Suspense fallback={<div>Loading...</div>}>
-					<Routes>
-						<Route path='/auth/login' element={<Login />} />
-						<Route path='/' element={<Index />} />
-						<Route path='/dashboard' element={<Dashboard />} />
-						<Route path='/inspect/atoms' element={<Inspect_Atoms />} />
-						<Route path='/inspect/molecules' element={<Inspect_Molecules />} />
-						<Route path='/inspect/organisms' element={<Inspect_Organisms />} />
-						<Route path='/inspect/widgets' element={<Inspect_Widgets />} />
-					</Routes>
+					{/* Use the AppRoutes component here */}
+					<AppRoutes />
 				</Suspense>
 			</Router>
 		</React.StrictMode>
---
+++ src/routes.tsx
@@ -0,0 +1,22 @@
+import { Route, Routes } from 'react-router-dom';
+import Index from './views/app/index';
+import Dashboard from './views/app/dashboard';
+import Login from './views/auth/login';
+import Inspect_Atoms from './views/inspect/atoms';
+import Inspect_Molecules from './views/inspect/molecules';
+import Inspect_Organisms from './views/inspect/organisms';
+import Inspect_Widgets from './views/inspect/widgets';
+
+const AppRoutes = () => (
+	<Routes>
+		<Route path='/auth/login' element={<Login />} />
+		<Route path='/' element={<Index />} />
+		<Route path='/dashboard' element={<Dashboard />} />
+		<Route path='/inspect/atoms' element={<Inspect_Atoms />} />
+		<Route path='/inspect/molecules' element={<Inspect_Molecules />} />
+		<Route path='/inspect/organisms' element={<Inspect_Organisms />} />
+		<Route path='/inspect/widgets' element={<Inspect_Widgets />} />
+	</Routes>
+);
+
+export default AppRoutes;
